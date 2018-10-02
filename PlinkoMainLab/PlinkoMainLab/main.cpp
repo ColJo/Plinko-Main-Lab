9ba47ff45bb55e1ef698fa2900d94bc2a90983d0
Coleman Johnston <colemantjohnston@outlook.com>
2018-10-02 16:48:42 -0600

    lots more stuff

--- PlinkoMainLab/PlinkoMainLab/main.cpp
+++ PlinkoMainLab/PlinkoMainLab/main.cpp
@@ -27,26 +27,7 @@ int main() {
 	double totalWinnings = 0.0;
 	double avgWinnings = 0.0;
 
-	cout << "Welcome to the Plinko simulator! ";
-	do {
-		cout << " Enter 3 to see options." << endl;
-		cout << endl;
-		cout << "Enter your selection now: ";
-		cin >> userInput;
-		cout << endl;
-		if (userInput != 3) {
-			cout << "Invalid selection. ";
-		}
-	} while (userInput != 3);
-
-	cout << endl;
-
-	cout << "Menu: Please select one of the following options:" << endl;
-	cout << endl;
-	cout << "1 - Drop a single chip into one slot" << endl;
-	cout << "2 - Drop multiple chips into one slot" << endl;
-	cout << "3 - Show the options menu" << endl;
-	cout << "4 - Quit the program";
+	cout << "Welcome to the Plinko simulator!  Enter 3 to see options.";
 
 	do {
 		cout << endl << endl;
@@ -70,76 +51,7 @@ int main() {
 				ballPosition = static_cast<double>(slotChoice);
 			}
 
-			for (int j = 0; j < numChips; j++) {
-				for (int i = 0; i < 12; i++) {
-					randNum = rand() % 2;
-					if (randNum == 0) {
-						if (ballPosition == 0) {
-							ballPosition += 0.5;
-						}
-						else {
-							ballPosition -= 0.5;
-						}
-					}
-					else {
-						if (ballPosition == 8) {
-							ballPosition -= 0.5;
-						}
-						else {
-							ballPosition += 0.5;
-						}
-					}
-
-					if (ballPosition == 0 || ballPosition == 8) {
-						winnings = 100.00;
-					}
-					else if (ballPosition == 1 || ballPosition == 7) {
-						winnings = 500.00;
-					}
-					else if (ballPosition == 2 || ballPosition == 6) {
-						winnings = 1000.00;
-					}
-					else if (ballPosition == 3 || ballPosition == 5) {
-						winnings = 0.00;
-					}
-					else if (ballPosition == 4) {
-						winnings = 10000.00;
-					}
-				}
-
-				totalWinnings += winnings;
-			}
-
-			cout << setprecision(2) << "Winnings: $" << winnings << endl;
-		}
-
-		// Menu input of 2
-		if (menuInput == 2) {
-			cout << endl << endl;
-			cout << "*** Drop multiple chips ***" << endl;
-			cout << endl;
-			cout << "How many chips do you want to drop (>0)? ";
-			cin >> numChips;
-			cout << endl << endl;
-
-			if (numChips <= 0)
-			{
-				cout << "Invalid number of chips.";
-				continue;
-			}
-
-			cout << "Which slot do you want to drop the chip in (0-8)? ";
-			cin >> slotChoice;
-			cout << endl << endl;
-
-			if (slotChoice < 0 || slotChoice > 8) {
-				cout << "Invalid slot.";
-			}
-			else {
-				ballPosition = static_cast<double>(slotChoice);
-			}
-
-			for (int i = 0; i < 12; i = i + 1) {
+			for (int i = 0; i < 12; i++) {
 				randNum = rand() % 2;
 				if (randNum == 0) {
 					if (ballPosition == 0) {
@@ -215,11 +127,99 @@ int main() {
 			cout << fixed << setprecision(1) << "Path: [" << position0 << ", " << position1 << ", " << position2 << ", "
 				<< position3 << ", " << position4 << ", " << position5 << ", " << position6 << ", " << position7 << ", "
 				<< position8 << ", " << position9 << ", " << position10 << ", " << position11 << "]" << endl;
-			cout << setprecision(2) << "Winnings: $" << winnings << endl;
+			cout << setprecision(2) << "Winnings: $" << winnings;
 		}
+		// Menu input of 2
+		else if (menuInput == 2) {
+			cout << endl << endl;
+			cout << "*** Drop multiple chips ***" << endl;
+			cout << endl;
+			cout << "How many chips do you want to drop (>0)? ";
+			cin >> numChips;
+			cout << endl << endl;
 
-		if (menuInput == 4) {
+			if (numChips <= 0)
+			{
+				cout << "Invalid number of chips.";
+				continue;
+			}
+
+			cout << "Which slot do you want to drop the chip in (0-8)? ";
+			cin >> slotChoice;
+			cout << endl << endl;
+
+			if (slotChoice < 0 || slotChoice > 8) {
+				cout << "Invalid slot.";
+			}
+			else {
+				ballPosition = static_cast<double>(slotChoice);
+			}
+
+			totalWinnings = 0.0;
+			for (int j = 0; j < numChips; j++) {
+				for (int i = 0; i < 12; i = i + 1) {
+					randNum = rand() % 2;
+					if (randNum == 0) {
+						if (ballPosition == 0) {
+							ballPosition += 0.5;
+						}
+						else {
+							ballPosition -= 0.5;
+						}
+					}
+					else {
+						if (ballPosition == 8) {
+							ballPosition -= 0.5;
+						}
+						else {
+							ballPosition += 0.5;
+						}
+					}
+
+					if (ballPosition == 0 || ballPosition == 8) {
+						winnings = 100.00;
+					}
+					else if (ballPosition == 1 || ballPosition == 7) {
+						winnings = 500.00;
+					}
+					else if (ballPosition == 2 || ballPosition == 6) {
+						winnings = 1000.00;
+					}
+					else if (ballPosition == 3 || ballPosition == 5) {
+						winnings = 0.00;
+					}
+					else if (ballPosition == 4) {
+						winnings = 10000.00;
+					}
+				}
+
+				totalWinnings += winnings;
+			}
+
+			cout << setprecision(2) << "Total winnings on " << numChips << " chips: $" << totalWinnings << endl;
+
+			avgWinnings = totalWinnings / numChips;
+			cout << "Average winnings per chip: $" << avgWinnings;
+		}
+		// Menu input of 3
+		else if (menuInput == 3) {
+			cout << endl;
+			cout << "Menu: Please select one of the following options:" << endl;
+			cout << endl;
+			cout << "1 - Drop a single chip into one slot" << endl;
+			cout << "2 - Drop multiple chips into one slot" << endl;
+			cout << "3 - Show the options menu" << endl;
+			cout << "4 - Quit the program";
+		}
+		// Menu input of 4
+		else if (menuInput == 4) {
+			cout << endl;
 			cout << "Goodbye!";
+		}
+		else {
+			cout << endl;
+			cout << "Invalid selection. Enter 3 to see options.";
+			continue;
 		}
 	} while (menuInput != 4);
 
