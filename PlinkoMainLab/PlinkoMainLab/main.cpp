b29eb924e26c9fca7620955ddd3e44dc6bbb6867
Coleman Johnston <colemantjohnston@outlook.com>
2018-10-02 14:33:45 -0600

    updated the menu

--- PlinkoMainLab/PlinkoMainLab/main.cpp
+++ PlinkoMainLab/PlinkoMainLab/main.cpp
@@ -4,14 +4,31 @@ using namespace std;
 
 int main() {
 	int userInput;
+	int menuInput;
 
-	cout << "Welcome to the Plinko simulator! Enter 3 to see options." << endl;
+	cout << "Welcome to the Plinko simulator! ";
+	do {
+		cout << " Enter 3 to see options." << endl;
+		cout << endl;
+		cout << "Enter your selection now: ";
+		cin >> userInput;
+		cout << endl;
+		if (userInput != 3){
+			cout << "Invalid selection. ";
+		}
+	} while (userInput != 3);
+
 	cout << endl;
 
-	cout << "Enter your selection now: ";
-	cin >> userInput;
-	cout << endl;
-	cout << endl;
+	if (userInput == 3) {
+		cout << "Menu: Please select one of the following options:" << endl;
+		cout << endl;
+		cout << "1 - Drop a single chip into one slot" << endl;
+		cout << "2 - Drop multiple chips into one slot" << endl;
+		cout << "3 - Show the options menu" << endl;
+		cout << "4 - Quit the program" << endl;
+	}
+	
 
 	system("pause");
 	return 0;
