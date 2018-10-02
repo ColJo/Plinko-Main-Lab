61c53a0cf829c802907fcf9e6b769c677a8123d1
Coleman Johnston <colemantjohnston@outlook.com>
2018-10-02 14:37:04 -0600

    I removed the not needed if statement

--- PlinkoMainLab/PlinkoMainLab/main.cpp
+++ PlinkoMainLab/PlinkoMainLab/main.cpp
@@ -20,14 +20,13 @@ int main() {
 
 	cout << endl;
 
-	if (userInput == 3) {
-		cout << "Menu: Please select one of the following options:" << endl;
-		cout << endl;
-		cout << "1 - Drop a single chip into one slot" << endl;
-		cout << "2 - Drop multiple chips into one slot" << endl;
-		cout << "3 - Show the options menu" << endl;
-		cout << "4 - Quit the program" << endl;
-	}
+	cout << "Menu: Please select one of the following options:" << endl;
+	cout << endl;
+	cout << "1 - Drop a single chip into one slot" << endl;
+	cout << "2 - Drop multiple chips into one slot" << endl;
+	cout << "3 - Show the options menu" << endl;
+	cout << "4 - Quit the program" << endl;
+	
 	
 
 	system("pause");
