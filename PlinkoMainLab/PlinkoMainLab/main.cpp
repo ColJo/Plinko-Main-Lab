e566a9ce5baefb6f593aace08303ead9de613b2a
Coleman Johnston <colemantjohnston@outlook.com>
2018-10-02 14:22:03 -0600

    stuff

--- PlinkoMainLab/PlinkoMainLab/main.cpp
+++ PlinkoMainLab/PlinkoMainLab/main.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 
 int main() {
+	int userInput;
+
 	cout << "Welcome to the Plinko simulator! Enter 3 to see options." << endl;
 	cout << endl;
 
 	cout << "Enter your selection now: ";
+	cin >> userInput;
+	cout << endl;
+	cout << endl;
 
 	system("pause");
 	return 0;
